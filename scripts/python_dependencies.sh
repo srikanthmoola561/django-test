084fbdf835b40602c7fecdd4cb60773eb2d3a466
srikanth moola <reachmoola@gmail.com>
2024-11-08 18:40:31 -0500

    added new real files

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-virtualenv /home/ubuntu/env
-source /home/ubuntu/env/bin/activate
-pip install -r /home/ubuntu/blogprojectdrf/requirements.txt
+sudo chown -R ubuntu:ubuntu ~/Project_folder_name
+virtualenv /home/ubuntu/Project_folder_name/venv
+source /home/ubuntu/Project_folder_name/venv/bin/activate
+pip install -r /home/ubuntu/Project_folder_name/requirements.txt
